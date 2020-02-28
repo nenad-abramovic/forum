56b33b09e5342e436a56caf790e329de31f440f2
Ненад Абрамовић <nenadabramovic@outlook.com>
2020-02-28 21:58:46 +0100

    Minor changes

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -7,10 +7,18 @@ const Message = ({ message }) => {
 
   return (
     <div className={styles.message}>
-      <p className={styles.info}>На дан {datetime.toLocaleDateString()} у {datetime.toLocaleTimeString()}, <Link to={`/profile/${123}`}>{message.username.toString()}</Link> је написао:</p>
+      <p className={styles.info}>
+        На дан
+        {datetime.toLocaleDateString('sr-rs', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}
+        у
+        {datetime.toLocaleTimeString('sr-rs')},
+        <Link to={`/profile/${message.user_id}`}>
+          {message.username.toString()}
+        </Link>
+        је написао:
+      </p>
       <br />
       <p>{message.message.toString()}</p>
-      <hr />
     </div>
   );
 };
