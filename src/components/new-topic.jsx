adce9e465d3ebfa87989c7518cadc028f4d8aee5
Ненад Абрамовић <nenadabramovic@outlook.com>
2020-02-28 22:39:39 +0100

    Minor changes

--- src/components/new-topic.jsx
+++ src/components/new-topic.jsx
@@ -12,16 +12,22 @@ const NewTopic = () => {
 
   const handleClick = (e) => {
     e.preventDefault();
-    if (message === '' || topicTitle === '') {
-      return setInfoMessage('Topic Title or first message can\'t be empty!');
+    if (message === '' && topicTitle === '') {
+      return setInfoMessage('Унеси наслов теме и прву поруку.');
+    }
+    if (message === '') {
+      return setInfoMessage('Унеси прву поруку.');
+    }
+    if (topicTitle === '') {
+      return setInfoMessage('Унеси наслов теме.');
     }
 
     addNewTopic(getUser().user_id, topicTitle)
       .then(data => {
-        if (data.success === true) {
+        if (data.success) {
           sendMessage(getUser().username, data.topic.topic_id, message)
             .then(msg_data => {
-              if (msg_data.success === true) {
+              if (msg_data.success) {
                 history.push({
                   pathname: `/topic/${data.topic.topic_id}`,
                   state: { topic: data.topic }
@@ -29,18 +35,18 @@ const NewTopic = () => {
               }
             });
         }
-      })
+      });
   };
 
   return (
     <form className={styles.form}>
-      <h4>New Topic</h4>
-      <label htmlFor="topic-title">Title:</label>
-      <input className={styles.input} type="text" id="topic-title" placeholder="Enter title" value={topicTitle} onChange={e => setTopicTitle(e.target.value)} onBlur={e => setTopicTitle(e.target.value.trim())} />
-      <label htmlFor="message">Message:</label>
-      <input className={styles.input} type="text" id="message" placeholder="Enter message" value={message} onChange={e => setMessage(e.target.value)} onBlur={e => setMessage(e.target.value.trim())} />
-      <p>{infoMessage}</p>
-      <input className={styles.inputBtn} type="submit" value="Add Topic" onClick={handleClick} />
+      <h4>Додај нову тему</h4>
+      <label htmlFor="topic-title">Наслов:</label>
+      <input className={styles.input} type="text" id="topic-title" value={topicTitle} onChange={e => setTopicTitle(e.target.value)} onBlur={e => setTopicTitle(e.target.value.trim())} />
+      <label htmlFor="message">Прва порука:</label>
+      <input className={styles.input} type="text" id="message" value={message} onChange={e => setMessage(e.target.value)} onBlur={e => setMessage(e.target.value.trim())} />
+      <p className={styles.infoMessage}>{infoMessage}</p>
+      <input className={styles.button} type="submit" value="Додај тему" onClick={handleClick} />
     </form>
   );
 };
